94e608a207900bbd7bd8e4c55a0c541c8b1fcb7e
akinoyor <ec2-user@ip-172-31-8-171.ap-northeast-1.compute.internal>
2024-03-17 14:42:36 +0000

    [add]books index 制作ちゅう

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,30 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def index
+    @user = current_user
+    @book = Book.new
+    @books = @user.books
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.user_id = current_user.id
+    book.save
+    redirect_to user_path(current_user.id)
   end
 
   def show
   end
 
   def edit
+  end
+
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
   end
 end
