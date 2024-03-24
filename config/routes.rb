de9db385ce183fbf6993cf28a02142b790da17bd
akinoyor <ec2-user@ip-172-31-8-171.ap-northeast-1.compute.internal>
2024-03-24 03:11:13 +0000

    [add]ユーザ情報編集画面

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get 'homes/about',as:'about'
 
-  resources :users, only:[:index,:show,:edit]
+  resources :users, only:[:index,:show,:edit,:update]
   resources :books, only:[:index, :show, :edit, :new,:create]
 
 
